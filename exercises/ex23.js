4594e0881ece4dc65af4b925654a2a5a9c9cf8c7
jamie <jamie927@gmail.com>
2026-01-28 18:47:11 -0800

    exercise 23 completed

--- exercises/ex23.js
+++ exercises/ex23.js
@@ -6,3 +6,22 @@ Under the first, write a while loop that prints out each item of ingredients.
 Under the second, write a for loop that does the same thing.
 And under the third, write any loop, while or for that prints out each item of ingredients but backwards (that is, the first should be "bananas" and the last "eggs").
 */
+
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+console.log("-----the first-----");
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+
+console.log("-----the second-----");
+for (let i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+
+console.log("-----the third-----");
+for (let i = ingredients.length - 1; i >= 0; i--) {
+  console.log(ingredients[i]);
+}
